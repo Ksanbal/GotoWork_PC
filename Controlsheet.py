da73d45c73256d0b9cd5835b7c467b3d874465ab
김산발 <hyungyun96@gmail.com>
2019-12-06 00:36:40 +0900

    구글 시트와 연결하는 코드를 작성하였습니다.

---
+++ Controlsheet.py
@@ -0,0 +1,19 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+from APIs import sheet_url
+
+scope = [
+    'https://spreadsheets.google.com/feeds',
+    'https://www.googleapis.com/auth/drive',
+]
+filename = 'gotowork-pc-9ba003ba9034.json'
+
+credentials = ServiceAccountCredentials.from_json_keyfile_name(filename, scope)
+gc = gspread.authorize(credentials)
+
+spreadsheet_url = sheet_url
+
+doc = gc.open_by_url(spreadsheet_url)
+
+# 구글 스프레드 시트 테스트
+
