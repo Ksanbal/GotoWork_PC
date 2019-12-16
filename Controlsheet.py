5bbbd65e9db9ab4fb22cce1ab9feb1d9fe51a818
김산발 <hyungyun96@gmail.com>
2019-12-16 18:35:27 +0900

    파일 경로가 수정되었습니다.

--- Controlsheet.py
+++ Controlsheet.py
@@ -1,12 +1,11 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from apis import sheet_url
+from apis import sheet_url, filename
 
 scope = [
     'https://spreadsheets.google.com/feeds',
     'https://www.googleapis.com/auth/drive',
 ]
-filename = 'gotowork-pc-9ba003ba9034.json'
 
 credentials = ServiceAccountCredentials.from_json_keyfile_name(filename, scope)
 gc = gspread.authorize(credentials)
